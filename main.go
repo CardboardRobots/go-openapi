e80ea6eb668835eca26dcfb9f00bcb21c31383de
Sean Johnson <sean.johnson.arizona@gmail.com>
2022-07-29 17:01:00 -0700

    Fixed flags.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"embed"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/cardboardrobots/go-openapi/core"
@@ -14,23 +13,25 @@ import (
 var content embed.FS
 
 func main() {
-
-	ctx := context.Background()
-	options := GetOptions()
+	options := getOptions()
 
 	log.SetPrefix("[OpenApi Loader] ")
 	log.Println("Starting...")
 
+	ctx := context.Background()
 	core.ParseDocument(ctx, content, options)
 }
 
-func GetOptions() core.ParseOptions {
-	options := core.ParseOptions{
-		Input:   *flag.String("i", "openapi.yml", "Input location"),
-		Output:  *flag.String("o", "gen.go", "Output location"),
-		Package: *flag.String("p", "definition", "Package name"),
-	}
+func getOptions() core.ParseOptions {
+	Input := flag.String("i", "openapi.yml", "Input location")
+	Output := flag.String("o", "gen.go", "Output location")
+	Package := flag.String("p", "definition", "Package name")
+
 	flag.Parse()
-	fmt.Printf("%v", options)
-	return options
+
+	return core.ParseOptions{
+		Input:   *Input,
+		Output:  *Output,
+		Package: *Package,
+	}
 }
